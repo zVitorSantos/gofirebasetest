eef6f7f88ee32bdd5c847f189fb43c2db07a27b1
zVitorSantos <vitorsantos4736@gmail.com>
2024-05-09 22:00:56 -0300

    💩 ops

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,22 @@
 import { createApp } from 'vue';
-import './style.css'
+import './style.css';
 import App from './App.vue';
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
+import dotenv from 'dotenv';
+
+// Carregar as variáveis de ambiente do arquivo .env
+dotenv.config();
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD0g3wQjw73bGPpU2Id1ZrjKMtx73mo6-w",
-    authDomain: "firetest-98x8.firebaseapp.com",
-    projectId: "firetest-98x8",
-    storageBucket: "firetest-98x8.appspot.com",
-    messagingSenderId: "877385955451",
-    appId: "1:877385955451:web:f526a642c515c39b328ade",
-    measurementId: "G-60GCBFQD45"
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -20,4 +24,3 @@ const firebaseApp = initializeApp(firebaseConfig);
 const analytics = getAnalytics(firebaseApp);
 
 createApp(App).mount('#app');
-
